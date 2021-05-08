ae477b14d5dc4f0b28c28a56ab75821c79b4d9c6
Tim Busuttil <timothyjbusuttil@gmail.com>
2021-05-08 15:16:49 +1000

    init svg rendering

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,28 @@
     <h1>Imse Editor</h1>
     <textarea v-model="input" name="name" rows="8" cols="80"></textarea>
     <pre>{{ parsedInput }}</pre>
+    <input type="color" name="fgCol" v-model="fgCol">
+    <label for="fgCol">Foreground Colour</label><br>
+    <input type="color" name="bgCol" v-model="bgCol">
+    <label for="bgCol">Background Colour</label><br>
+    <input type="checkbox" id="showAnnotations" v-model="showAnnotations">
+    <label for="showAnnotations">Show Annotations</label><br>
+    <button @click="exportSvg">Export SVG</button><br>
+    <svg
+      ref="svgContainer"
+      xmlns="http://www.w3.org/2000/svg" version="1.1"
+      width="500"
+      height="500"
+      :style="{ backgroundColor: bgCol }"
+    >
+      <g v-for="(line, i) in parsedInput" :transform="`translate(${i * 100}, 0)`" :key="i" >
+        <g v-for="(character, j) in line" :transform="`translate(0, ${j * 60})`" :key="j">
+          <rect  width="90" height="50" :fill="fgCol" />
+          <text v-show="showAnnotations" x="20" y="35" fill="red">{{ character }}</text>
+        </g>
+      </g>
+      <image href="@/assets/characters/original/k.svg" />
+    </svg>
   </div>
 </template>
 
@@ -13,6 +35,9 @@ export default {
     return {
       input: 'jemboke gonboke\nki maika t:a',
       validCharacters: [' ', 'a', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'j:', 'k', 'k:', 'l', 'm', 'n', 'n:', 'o', 'r', 's', 's:', 't', 't:', 'v'],
+      fgCol: '#000000',
+      bgCol: '#ffffff',
+      showAnnotations: true,
     }
   },
   computed: {
@@ -27,6 +52,11 @@ export default {
 
       // remove any hyphen characters
       input = input.replaceAll('-', '');
+
+      // TODO: accept some character aliases
+      // accept y as an alias for j:
+      input = input.replaceAll('y', 'j:');
+      // decide whether c & c: or k & k: is canonical, and accept one as alias for the other
 
       // split into lines to return in array
       const lines = input.split('\n');
@@ -74,9 +104,24 @@ export default {
       });
     },
   },
+  methods: {
+    exportSvg() {
+      const svgData = this.$refs.svgContainer.outerHTML;
+      const svgBlob = new Blob([svgData], {type:"image/svg+xml;charset=utf-8"});
+      const svgUrl = URL.createObjectURL(svgBlob);
+      const downloadLink = document.createElement("a");
+      downloadLink.href = svgUrl;
+      downloadLink.download = "imse export.svg";
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    }
+  }
 }
 </script>
 
 <style>
-
+html {
+  background: #fce4ec;
+}
 </style>
